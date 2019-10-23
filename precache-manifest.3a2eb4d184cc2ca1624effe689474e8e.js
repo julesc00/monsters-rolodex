633ead5c9be5ff54513d092c9004722fae3604b2
Julio Briones <juliobriones@pop-os.localdomain>
2019-10-23 05:39:14 -0500

    Updates

---
+++ precache-manifest.3a2eb4d184cc2ca1624effe689474e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771614664979f8d0dcc1b12d3b6f6fc5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc5d0365a74ce5ce64f5",
+    "url": "/monsters-rolodex/static/css/main.497c9a38.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "dc5d0365a74ce5ce64f5",
+    "url": "/monsters-rolodex/static/js/main.420a8285.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
